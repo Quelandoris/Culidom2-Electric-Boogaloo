4ee9fca352e8beed186badde63608fa8e8edb597
Antonio Diaz <antonio.diaz.02071998@gmail.com>
2018-12-19 02:54:18 -0600

    Implemented better Tech UI updating

---
+++ CuliDom/CulinaryDomination/Assets/Scripts/Tech/TechTreeUIUpdate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TechTreeUIUpdate : MonoBehaviour {
+
+    public List<TechPurchase> techs = new List<TechPurchase>();
+
+    
+
+    public void OnEnable()
+    {
+        for (int i = 0; i < techs.Count; i++) {
+            if (GameController.Instance().activePlayer.HaveTech(techs[i].thisTech)) { // will return true if player has it
+                techs[i].techPicture.SetActive(true);
+            }
+        }
+    }
+
+    public void OnDisable()
+    {
+        for (int i = 0; i < techs.Count; i++)
+        {
+            techs[i].techPicture.SetActive(false);
+        }
+    }
+}
